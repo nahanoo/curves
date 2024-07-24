9b1c90efc4ef79e159c4ef593aeeeb200a8fa3fb
nahanoo <ericsven.u@gmail.com>
2024-07-24 10:36:44 +0200

    added simple meta data table

--- app.py
+++ app.py
@@ -1,39 +1,63 @@
-from dash import Dash, html, dash_table, dcc, callback, Output, Input
 import pandas as pd
-from os.path import join
+from dash import Dash, dcc, html, Input, Output, dash_table, callback
 import plotly.express as px
-import plotly.graph_objects as go
+from os.path import join
 
+# Load the data
 df = pd.read_csv(join("data", "240623_growth_phenotyping", "at", "data_annotated.csv"))
+
+# Get unique carbon sources and species
+cs = list(set(df["carbon_source"]))
 species = list(set(df["species"]))
 
-cs = list(set(df["carbon_source"]))
-app = Dash()
+# Create the Dash app
+app = Dash(__name__)
 
 # App layout
-app.layout = [
-    html.Div(children="Example data"),
-    html.Hr(),
-    dcc.Dropdown(cs, "D-glucose", id="cs-dropdown"),
-    dcc.Dropdown(species, species[0], id="species-dropdown"),
-    dcc.Graph(figure={}, id="controls-and-graph"),
-]
+app.layout = html.Div(
+    [
+        html.Div(children="Example data"),
+        html.Hr(),
+        dcc.Dropdown(cs, "D-glucose", id="cs-dropdown"),
+        dcc.Dropdown(species, species[0], id="species-dropdown"),
+        dcc.Graph(figure={}, id="controls-and-graph"),
+        dash_table.DataTable(
+            id="table",
+            columns=[
+                {"name": i, "id": i}
+                for i in df[
+                    ["species", "carbon_source", "concentration", "project"]
+                ].columns
+            ],
+        ),
+    ]
+)
 
 
 # Add controls to build the interaction
 @callback(
-    Output(component_id="controls-and-graph", component_property="figure"),
-    Input(component_id="cs-dropdown", component_property="value"),
-    Input(component_id="species-dropdown", component_property="value"),
+    [
+        Output(component_id="controls-and-graph", component_property="figure"),
+        Output(component_id="table", component_property="data"),
+    ],
+    [
+        Input(component_id="cs-dropdown", component_property="value"),
+        Input(component_id="species-dropdown", component_property="value"),
+    ],
 )
 def update_carbon_source(col_chosen, species_chosen):
     df_filtered = df[
         (df["carbon_source"] == col_chosen) & (df["species"] == species_chosen)
     ].sort_values("Time")
     fig = px.line(df_filtered, x="Time", y="OD", line_group="linegroup")
-    return fig
+    table_data = (
+        df_filtered[["species", "carbon_source", "concentration", "project"]]
+        .drop_duplicates()
+        .to_dict("records")
+    )
+    return fig, table_data
 
 
-# Run the app
+# Run the Dash app
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run_server(debug=True)
