b84dd778655ad28c958c46fd220b079fadb1f4d1
Prajwal Padmanabha <pprajwal122@gmail.com>
2024-07-29 14:20:13 +0200

    added comments and updated variable names

--- app_new.py
+++ app_new.py
@@ -5,16 +5,17 @@ from os.path import join
 from functools import reduce
 import os
 
-parsedDataDir = "data/parsed_csvs"
-parsedProjects = next(os.walk(parsedDataDir))[1]
-parsedProjects.sort()
+# Data directory with the subfolders for each project
+parsed_data_dir = "data/parsed_csvs"
+parsed_projects = next(os.walk(parsed_data_dir))[1]
+parsed_projects.sort()
 
-project = parsedProjects[0]
+# Choose the first project as default
+project = parsed_projects[0]
 
 # Load the data
 pooled_df_joint_metadata = pd.DataFrame()
-
-for project in parsedProjects:
+for project in parsed_projects:
     df_species = pd.read_csv(join("data", "parsed_csvs",project, project+"_species_data.csv"))
     df_carbon_source = pd.read_csv(join("data", "parsed_csvs", project,project+"_carbon_source_data.csv"))
     df_technical = pd.read_csv(join("data", "parsed_csvs", project,project+"_technical_data.csv"))
@@ -22,7 +23,10 @@ for project in parsedProjects:
     df_run = pd.read_csv(join("data", "parsed_csvs", project,project+"_run_data.csv"))
     df_inhibitor = pd.read_csv(join("data", "parsed_csvs",project, project+"_inhibitor_data.csv"))
 
+    # Merge data with expID as common columns
     df_joint_technical = df_run.merge(df_technical, on="expID", how="outer")
+
+    # Merge all metadata with linegroup as common reference
     df_joint_metadata = reduce(lambda x,y: pd.merge(x,y, on='linegroup', how='outer'), [df_joint_technical,df_species,df_carbon_source,df_comments])
     pooled_df_joint_metadata = pd.concat([pooled_df_joint_metadata,df_joint_metadata])
 
@@ -39,17 +43,16 @@ species.sort()
 app = Dash(__name__)
 app.css.append_css({"external_url": "assets/style_new.css"})
 
-# App layout
-
+# App layout with dropdowns for project, carbon source and species 
 app.layout = html.Div(
     className="container",
     children=[
-        html.H1(children="Example Data"),
+        html.H1(children="Growth Phenotyping"),
         html.Hr(),
         html.Div(
             className="dropdown",
             children=[
-                dcc.Dropdown(parsedProjects, parsedProjects[0], id="proj-dropdown"),
+                dcc.Dropdown(parsed_projects, parsed_projects[0], id="proj-dropdown"),
                 dcc.Dropdown(cs, cs[0], id="cs-dropdown"),
                 dcc.Dropdown(species, species[0], id="species-dropdown"),
             ]
@@ -89,15 +92,19 @@ app.layout = html.Div(
     ],
 )
 def update_carbon_source(proj_chosen,col_chosen, species_chosen):
+    # Read only data for selected project, species and carbon source
+    df_data = pd.read_csv(join(parsed_data_dir,proj_chosen, proj_chosen+"_measurement_data.csv"))
 
-    df_data = pd.read_csv(join(parsedDataDir,proj_chosen, proj_chosen+"_measurement_data.csv"))
+    # Filter the appropriate linegroups and associated data
     lg_species_chosen = pooled_df_joint_metadata[pooled_df_joint_metadata["species"] == species_chosen]["linegroup"]
     lg_carbon_source_chosen = pooled_df_joint_metadata[pooled_df_joint_metadata["carbon_source"] == col_chosen]["linegroup"]
     filtered_lg = list(set(lg_species_chosen) & set(lg_carbon_source_chosen))
     df_data_filtered = df_data[df_data["linegroup"].isin(filtered_lg)].sort_values(by="time")
   
+    # Plot the data
     fig = px.line(df_data_filtered, x="time", y="measurement", line_group="linegroup")
 
+    # Add chosen metadata as table
     filtered_metadata = pooled_df_joint_metadata[pooled_df_joint_metadata["linegroup"].isin(filtered_lg)]
     table_data = (
         filtered_metadata[to_show_in_table]
